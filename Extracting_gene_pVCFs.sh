d7e100e085c8a3d9a10c6537380e007c9a8ec55b
Ella Davyson <elladavyson@gmail.com>
2024-11-01 16:20:49 +0000

    Normalising all pVCFs first to avoid doing that step for each gene

--- Extracting_gene_pVCFs.sh
+++ Extracting_gene_pVCFs.sh
@@ -1,4 +1,17 @@
-input_file="test_cluster_UKB_pVCF.tsv"
+input_file="FADS_genes_remaining_UKB_pVCF.tsv"
+
+# Normalise all the pVCF files 
+
+for vcf_file in *.vcf.gz; do
+echo "$vcf_file"
+base_name=$(basename "$vcf_file" .vcf.gz)
+norm_vcf="${base_name}_norm.vcf.gz"
+bcftools norm -f GRCh38_full_analysis_set_plus_decoy_hla.fa -m -any -Oz -o "$norm_vcf" "$vcf_file"
+bcftools index --tbi "$norm_vcf"
+echo "Normalized $vcf_file -> $norm_vcf"
+echo "-----------------"
+done 
+
 
 while IFS=$'\t' read -r hgnc_symbol chr start_position end_position strand ensembl_gene_id ensembl_transcript_id transcript_is_canonical covering_files; do
 
@@ -12,20 +25,19 @@ while IFS=$'\t' read -r hgnc_symbol chr start_position end_position strand ensem
     echo "VCF files for $hgnc_symbol:"
   # For each VCF file 
     for vcf_file in "${vcf_files[@]}"; do
-  # Get the base name for file tracking (i.e the field ID in UKB)
+  # Specify file names
     base_name=$(basename "$vcf_file" .vcf.gz)
-  # Define the output file names (one VCF and one TSV)
+    norm_file="${base_name}_norm.vcf.gz"
     output_file="${base_name}_norm_${hgnc_symbol}.vcf.gz"
     variant_output_file="${base_name}_${hgnc_symbol}_variants.tsv"
         echo "$vcf_file"
+        echo "$norm_file"
   # Filter out the gene region present in the VCF file, normalise and index
-        bcftools view -r "$region" "$vcf_file" -Ou |
-        bcftools norm -f GRCh38_full_analysis_set_plus_decoy_hla.fa -m -any -Oz -o $output_file
+        bcftools view -r "$region" "$norm_file" -0z -o $output_file
         bcftools index --tbi $output_file
-        echo "Normalised, left-aligned and biallelic sites for $hgnc_symbol processed from $vcf_file and saved to $output_file"
+        echo "Processed region $region for gene $hgnc_symbol processed from $norm_file and saved to $output_file"
   # Query the variants for Allele Frequency etc 
         bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t%FILTER\t%INFO/AC\t%INFO/AN\n' $output_file > $variant_output_file
-        dx upload --path "$DX_PROJECT_CONTEXT_ID:" $variant_output_file
     done
     echo "===================="
 
