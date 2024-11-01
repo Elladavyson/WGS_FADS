2bbbf91ed1c26406e7d74588f412bba64420ef0c
Ella Davyson <elladavyson@gmail.com>
2024-11-01 10:28:23 +0000

    Get file numbers

--- pVCF_ref_blocks.r
+++ pVCF_ref_blocks.r
@@ -1,6 +1,8 @@
 #
+.libPaths(c(.libPaths(), "/exports/igmm/eddie/GenScotDepression/users/edavyson/x86_64-pc-linux-gnu-library/4.4"))
 library(dplyr)
 library(data.table)
+library(biomaRt)
 
 # From https://biobank.ndph.ox.ac.uk/ukb/refer.cgi?id=2009
 # wget on EDDIE and copied to scratch 
@@ -11,18 +13,61 @@ library(data.table)
 # FADS3 gene: 61,873,519-61,892,051
 # MYRF gene: 61,752,636-61,788,518 
 # FEN1 gene: 61,792,911-61,797,238
+# TMEM258: 61768501-61792802
 
-gene_positions <- data.frame(
-  Gene = c("FADS1", "FADS2", "FADS3", "MYRF", "FEN1"),
-  Start_Position = c(61799627, 61792980, 61873519, 61752636, 61792911),
-  End_Position = c(61829318, 61867354, 61892051, 61788518, 61797238)
-)
+# The positions I used in the browser was from the UCSC browser 
+# Extract from Ensembl Browser
+# Specify the Ensembl dataset you want to use
+ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+
+attributes <- c("hgnc_symbol", "chromosome_name", "start_position", "end_position", "strand")
+
+# Retrieve gene information
+gene_info <- getBM(attributes = attributes, 
+                   mart = ensembl)
+fads_genes <- gene_info %>% filter(hgnc_symbol %in% c("FADS1", "FADS2", "FADS3", "FEN1", "TMEM258", "MYRF"))
+# Transcript information (canonical transcript?)
+transcript_attributes <- c("ensembl_gene_id", "ensembl_transcript_id", "transcript_is_canonical")
+transcript_info <- getBM(attributes = c(attributes, transcript_attributes), 
+                         filters = "hgnc_symbol", 
+                         values = fads_genes$hgnc_symbol, 
+                         mart = ensembl)
+fads_gene_transcript <- transcript_info %>% filter(transcript_is_canonical==1)
+gene_positions <- fads_gene_transcript %>% 
+mutate(chromosome_name = paste0("chr", chromosome_name)) %>% 
+rename(chr=chromosome_name)
+
+write.table(gene_positions, "Resources/FADS_cluster_genes.tsv", sep = "\t", row.names = F, quote = F)
 
 pvcf_ref <- read.table('Resources/dragen_pvcf_coordinates.csv', sep = ',', header = T)
-pvcf_chr11 <- pvcf_ref %>% filter(chromosome == 'chr11')
-pvcf_chr11 %>% filter(starting_position > 61700000) %>% head()
+# Calculate the end position of each file (the starting position of the next row -1)
+pvcf_ref <- pvcf_ref %>% 
+filter(chromosome == "chr11") %>% 
+arrange(starting_position) %>% 
+mutate(end = lead(starting_position, n = 1, default = NA) -1)
 
+# Filter for the files which start BEFORE the end of the gene 
+# OR end AFTER the start of the gene
+
+find_covering_files <- function(gene_start, gene_end, vcf_info) {
+    vcf_files <- vcf_info %>% filter((starting_position <= gene_end) & (end >= gene_start)) %>% 
+    pull(filename)
+    return(vcf_files)
+}
+# Getting the VCF file(s) for each gene 
+gene_coverage <- gene_positions %>% 
+rowwise() %>%
+mutate(covering_files = list(find_covering_files(start_position, end_position, pvcf_ref))) %>% ungroup()
+
+# Coverting to a character vector
+gene_coverage <- gene_coverage %>%
+  mutate(covering_files = sapply(covering_files, function(x) paste(x, collapse = ":")))
+
+# Get the numbers of the files 
+file_nums <- c(str_split(fads$covering_files, ":")) %>% unlist() %>% unique() %>% sub(".*b(\\d+).*", "\\1", .) %>% as.numeric() %>% sort()
+
+write.table(gene_coverage, "Resources/FADS_cluster_UKB_pVCF.tsv", sep = "\t", row.names = F, quote = F)
 # The file ukb24310_c11_b3090_v1.vcf.gz has a starting position of 61799029
 # The file ukb24310_c11_b3091_v1.vcf.gz has a starting position of 61819030
 
-# File with information on FADS cluster is b3090_v1
+# File with information on FADS cluster is b3090_v1 and b3091_v1
