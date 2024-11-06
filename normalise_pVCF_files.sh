fb37960762c13d90c0e3e195295e70b20cafc59b
Ella Davyson <elladavyson@gmail.com>
2024-11-06 17:22:38 +0000

    changing file num

--- normalise_pVCF_files.sh
+++ normalise_pVCF_files.sh
@@ -13,14 +13,14 @@ done
 
 run_normalise="bash normalise_pVCF_files.sh"
  dx run swiss-army-knife \
- -iin="/Bulk/DRAGEN\ WGS/DRAGEN\ population\ level\ WGS\ variants,\ pVCF\ format\ [500k\ release]/chr11/ukb24310_c11_b3094_v1.vcf.gz.tbi" \
- -iin="/Bulk/DRAGEN\ WGS/DRAGEN\ population\ level\ WGS\ variants,\ pVCF\ format\ [500k\ release]/chr11/ukb24310_c11_b3094_v1.vcf.gz" \
+ -iin="/Bulk/DRAGEN\ WGS/DRAGEN\ population\ level\ WGS\ variants,\ pVCF\ format\ [500k\ release]/chr11/ukb24310_c11_b3091_v1.vcf.gz.tbi" \
+ -iin="/Bulk/DRAGEN\ WGS/DRAGEN\ population\ level\ WGS\ variants,\ pVCF\ format\ [500k\ release]/chr11/ukb24310_c11_b3091_v1.vcf.gz" \
  -iin="/Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/helper_files/GRCh38_full_analysis_set_plus_decoy_hla.dict" \
  -iin="/Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/helper_files/GRCh38_full_analysis_set_plus_decoy_hla.fa" \
  -iin="/Bulk/Exome\ sequences/Exome\ OQFE\ CRAM\ files/helper_files/GRCh38_full_analysis_set_plus_decoy_hla.fa.fai" \
  -iin="/Code/normalise_pVCF_files.sh" \
  -icmd="${run_normalise}" \
- --tag="normalise_3094" \
+ --tag="normalise_3091" \
  --instance-type "mem1_hdd1_v2_x36" \
  --destination="/Output/" \
  --brief --yes
