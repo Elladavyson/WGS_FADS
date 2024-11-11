1fe2d9745d55750e73772dbd56ad6b4641dc988a
Ella Davyson <elladavyson@gmail.com>
2024-11-11 13:33:17 +0000

    Filtering to unrelated individuals of european ancestry with < 0.1 missingness

---
+++ sample_filters.R
@@ -0,0 +1,27 @@
+library(dplyr)
+library(optparse)
+
+parse <- OptionParser()
+args=commandArgs(trailingOnly = TRUE)
+opt <- parse_args(OptionParser(option_list=option_list), args = args)
+
+gene <- opt$gene
+
+# read in the missing file
+print(paste0("Reading in the missingness file for the ", gene, " from ", gene, "_missing.miss"))
+missing <- read.table(paste0(gene, "_missing.imiss"), header = T)
+table(missing$F_MISS < 0.1)
+print(paste0("There were ", missing %>% filter(F_MISS > 0.1) %>% nrow(), " individuals with > 0.1 missingness"))
+
+# Read in the list of unrelated individuals of european ancestry
+
+unrelated <- read.table("ukb_sqc_qc_WhiteBritishPCs_addPrunedRels_noPGC_noGenScot_v2.id")
+
+# Get intersection of the samples in the missing and the samples here 
+
+unrelated <- merge(unrelated, missing, by.x = "V1", by.y = "INDV")
+
+# write out the unrelated IDs
+
+readr::write_lines(unrelated$V1, paste0("unrelated_nomiss_", gene, "_idlist.id"))
+
