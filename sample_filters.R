b32414ecf39995dabca1eeef9e96a544494d9e85
Ella Davyson <elladavyson@gmail.com>
2024-11-12 12:47:37 +0000

    save table of QC numbers

--- sample_filters.R
+++ sample_filters.R
@@ -34,17 +34,20 @@ unrelated <- read.table("ukb_sqc_qc_WhiteBritishPCs_addPrunedRels_noPGC_noGenSco
 
 # Get intersection of the samples in the missing and the samples here 
 
-unrelated <- merge(unrelated, missing, by.x = "V1", by.y = "INDV")
+unrelated_missing <- merge(unrelated, missing, by.x = "V1", by.y = "INDV")
 print(paste0("Unrelated samples of european ancestry with WGS QC'd data for ", gene,": ", nrow(unrelated)))
 
-unrelated <- unrelated %>% filter(F_MISS <= 0.1)
-print(paste0("Unrelated samples of european ancestry and <= 0.1 missingness with WGS QC'd data for ", gene,": ", nrow(unrelated)))
+unrelated_missing <- unrelated_missing %>% filter(F_MISS <= 0.1)
+print(paste0("Unrelated samples of european ancestry and <= 0.1 missingness with WGS QC'd data for ", gene,": ", nrow(unrelated_missing)))
 # Also filter to those with a MDD phenotype (PGC3)
 mdd <- read.table("MajorDepression.ukb24262.2021-07.txt", header =T)
 mdd <- mdd %>% filter(MajDepr != -9)
-unrelated_mdd <- merge(unrelated, mdd, by.x = "V1", by.y = "IID")
+unrelated_mdd <- merge(unrelated_missing, mdd, by.x = "V1", by.y = "IID")
 print(paste0("Unrelated samples of european ancestry with WGS QC'd data for ", gene," and a MDD phenotype (PGC3)", nrow(unrelated_mdd)))
 
+# Write out table of numbers as the sample QC 
+
+sample_QC <- data.table(label= c("Total N", "Unrelated N", "Unrelated N with < 0.1 missingness", "Unrelated N with < 0.1 missingness and MDD phenotype"))
 # write out the unrelated IDs
 print(paste0("Writing out the sample list to unrelated_nomiss_mdd_", gene, "_idlist.id"))
 readr::write_lines(unrelated_mdd$V1, paste0("unrelated_nomiss_mdd_", gene, "_idlist.id"))
