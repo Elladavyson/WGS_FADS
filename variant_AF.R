ab6b4448a5ddb828cd4cf79953f1863920aa0ded
Ella Davyson <elladavyson@gmail.com>
2024-11-27 11:55:21 +0000

    Saving lists of variants with 0 allele counts and making a graph to show the numbers

--- variant_AF.R
+++ variant_AF.R
@@ -3,7 +3,7 @@ library(stringr)
 library(ggplot2)
 library(dplyr)
 library(ggpubr)
-
+library(readr) # Have to install this in the post workbench
 # dx download the variants 
 # dx download /Output/gene_VCF_variants/variants/*QC_variants.tsv
 # dx download /Input/FADS_cluster_UKB_pVCF.tsv
@@ -21,9 +21,12 @@ for (i in c(1:nrow(fads))) {
     AF = AC/AN)
     # Remove variants with an AC == 0 (these variants had a low allele count before sample QC)
     # And the participants which carried them have been removed- hence allele count == 0
+    variant_counts <- data.frame(GENE=gene, total = length(gene_table$chr_pos_ref_alt))
     gene_AC_zero <- gene_table %>% filter(AC==0) %>% pull(chr_pos_ref_alt)
+    variant_counts <- variant_counts %>% mutate(AC_zero=length(unique(gene_AC_zero)))
     gene_table <- gene_table %>% filter(AC != 0)
     gene_n <- length(unique(gene_table$chr_pos_ref_alt))
+    variant_counts <- variant_counts %>% mutate(AC_over0_postQC=gene_n)
     print(paste0("First variant:", gene_table$chr_pos_ref_alt[1], "\n Last variant:", tail(gene_table$chr_pos_ref_alt,1)))
     assign(paste0(gene, "_variants"), gene_table)
     frequency_counts <- gene_table %>% mutate(
@@ -58,6 +61,7 @@ range = case_when(
 assign(paste0(gene, "_AF_counts"), frequency_counts)
 assign(paste0(gene, "_AC_counts"), AC_counts)
 assign(paste0(gene, "_AC_zero_postqc"), gene_AC_zero)
+assign(paste0(gene, "_variant_counts"), variant_counts)
 }
 all_AF_counts <- rbind(MYRF_AF_counts, TMEM258_AF_counts, FEN1_AF_counts, FADS2_AF_counts,FADS1_AF_counts,  FADS3_AF_counts)
 all_AC_counts <- rbind(MYRF_AC_counts, TMEM258_AC_counts, FEN1_AC_counts, FADS2_AC_counts,FADS1_AC_counts,  FADS3_AC_counts)
@@ -81,3 +85,38 @@ gene_ac_plot <- ggplot(all_AC_counts, aes(x=range, y = n)) +
 
 ggsave(filename = "AF_barplots.png", gene_af_plot, width = 10, height = 8, device = "png", dpi = 300)
 ggsave(filename = "AC_barplots.png",gene_ac_plot, width = 10, height = 8, device = "png", dpi = 300)
+
+# Write out the list of variants with Allele Count == 0 in each gene 
+readr::write_lines(FADS1_AC_zero_postqc, "FADS1_postQC_ACzero.txt")
+readr::write_lines(FADS2_AC_zero_postqc, "FADS2_postQC_ACzero.txt")
+readr::write_lines(FADS3_AC_zero_postqc, "FADS3_postQC_ACzero.txt")
+readr::write_lines(FEN1_AC_zero_postqc, "FEN1_postQC_ACzero.txt")
+readr::write_lines(MYRF_AC_zero_postqc, "MYRF_postQC_ACzero.txt")
+readr::write_lines(TMEM258_AC_zero_postqc, "TMEM258_postQC_ACzero.txt")
+
+AC_counts <- rbind(FADS1_variant_counts, FADS2_variant_counts, FADS3_variant_counts, FEN1_variant_counts, MYRF_variant_counts, TMEM258_variant_counts)
+AC_counts_long <-  AC_counts  %>%
+    pivot_longer(cols = -GENE, 
+                 names_to = "Category", 
+                 values_to = "Count") %>%
+                 mutate(Category = factor(Category, levels = c("total", "AC_zero", "AC_over0_postQC")))
+
+ac_zero_counts_plt <- ggplot(AC_counts_long, aes(x=GENE, y = Count, fill = Category)) + 
+  geom_bar(stat="identity", position=position_dodge(width=0.8))+
+  geom_text(aes(label = Count), position = position_dodge(width = 0.8), vjust = -0.7, size = 2) +
+  labs(x = "GENE", y = "Count") + 
+  theme_minimal() +
+  theme(legend.position = "top")+ 
+  scale_fill_manual(
+    values = c("total" = "gray",
+    "AC_zero" = "red",
+    "AC_over0_postQC" = "#C77CFF"
+    ),
+labels = c("total"="Total variants (post-QC)",
+"AC_zero"="QC variants AC = 0",
+"AC_over0_postQC"="QC variants AC > 0" ))
+
+ggsave(filename = "AC_zero_postqc_barplot.png",ac_zero_counts_plt, width = 10, height = 8, device = "png", dpi = 300)
+
+# dx upload *_postQC_ACzero.txt
+# dx upload AC_zero_postqc_barplot.png
